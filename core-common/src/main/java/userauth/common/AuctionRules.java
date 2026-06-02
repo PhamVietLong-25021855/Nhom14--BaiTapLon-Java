1ed596ac8bb6faea1d49384ec569df7639186b43
Phạm Viết Long - 25021855 <phamvietlong0104@hmail.com>
2026-06-02 09:37:18 +0700

    chỉnh thời gian gia hạn từ 30s lên 1p

--- core-common/src/main/java/userauth/common/AuctionRules.java
+++ core-common/src/main/java/userauth/common/AuctionRules.java
@@ -3,7 +3,7 @@ package userauth.common;
 public final class AuctionRules {
     public static final int ADMIN_EARLY_CLOSE_COUNTS = 3;
     public static final int MAX_IMAGE_BYTES = 5 * 1024 * 1024;
-    public static final long ANTI_SNIPING_WINDOW_MS = 30_000L;
+    public static final long ANTI_SNIPING_WINDOW_MS = 60_000L;
     public static final int MAX_ANTI_SNIPING_EXTENSIONS = 3;
     public static final double MIN_BID_STEP_PERCENT = 0.01d;
     public static final double MAX_BID_STEP_PERCENT = 0.10d;
